a96b22537d81e61f8184132391117d36e3ce2b67
Tommycpg-ce6 <tommy.chuapg@gmail.com>
2024-09-28 12:29:49 +0800

    update main.tf

--- main.tf
+++ main.tf
@@ -176,3 +176,73 @@ resource "aws_lambda_permission" "api_gw" {
 
   source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
+
+data "aws_route53_zone" "zone" {
+  name = "sctp-sandbox.com"
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name       = "tommyce6.sctp-sandbox.com"
+  zone_id           = data.aws_route53_zone.zone.zone_id
+  validation_method = "DNS"
+}
+
+
+
+# Define the API Gateway Domain Name
+resource "aws_apigatewayv2_domain_name" "http_api" {
+  domain_name = "api.${local.name_prefix}.sctp-sandbox.com"
+
+  domain_name_configuration {
+    certificate_arn = module.acm.acm_certificate_arn  # Ensure this matches the output name
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+
+
+# API Mapping
+resource "aws_apigatewayv2_api_mapping" "example" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  domain_name = aws_apigatewayv2_domain_name.http_api.domain_name
+  stage       = aws_apigatewayv2_stage.default.name
+}
+
+# Route53 DNS Record
+resource "aws_route53_record" "http_api" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name     = "api.${local.name_prefix}.sctp-sandbox.com"
+  type     = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.http_api.domain_name
+    zone_id                = aws_apigatewayv2_domain_name.http_api.id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_apigatewayv2_domain_name" "http-api" {
+  domain_name = "api.${local.name_prefix}.sctp-sandbox.com"
+
+  domain_name_configuration {
+    certificate_arn = module.acm.acm_certificate_arn  # Ensure this matches the output name
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "http-api" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name     = "api.${local.name_prefix}.sctp-sandbox.com"
+  type     = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.http_api.domain_name
+    zone_id                = aws_apigatewayv2_domain_name.http_api.id
+    evaluate_target_health = true
+  }
+}
